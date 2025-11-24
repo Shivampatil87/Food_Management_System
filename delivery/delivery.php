1bb1cdf38d0d26f2368cce19f60056c8eeac3e25
Shivam Mahalle <shivammahalle41@gmail.com>
2025-11-24 14:17:07 +0530

    Initial commit

---
+++ delivery/delivery.php
@@ -0,0 +1,203 @@
+<?php
+session_start();
+$connection = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($connection, 'foodmanagement');
+if($_SESSION['city']==''){
+	echo "data is empty";
+}
+$name=$_SESSION['name'];
+$city=$_SESSION['city'];
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_URL,"http://ip-api.com/json");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+$result1=curl_exec($ch);
+$result2=json_decode($result1);
+// $city= $result->city;
+//echo $city;
+$id=$_SESSION['Did'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+    <link rel="stylesheet" href="../home.css">
+
+    <link rel="stylesheet" href="delivery.css">
+</head>
+<body>
+<header>
+        <div class="logo">Food <b style="color: #06C167;">Donate</b></div>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="openmap.php" >map</a></li>
+                <li><a href="deliverymyord.php" >myorders</a></li>
+                <li ><a href="../logout.php"  >Logout</a></li>
+                <!-- <li ><a href="../logout.php"  >Logout</a></li> -->
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <script>
+        hamburger=document.querySelector(".hamburger");
+        hamburger.onclick =function(){
+            navBar=document.querySelector(".nav-bar");
+            navBar.classList.toggle("active");
+        }
+    </script>
+<?php
+
+// echo var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=103.113.190.19')));
+// echo "Your city: {$city}\n";
+
+// $city = "<script language=javascript> document.write(geoplugin_city());</script>";
+// $scity=$city;
+?>
+    <style>
+        .itm{
+            background-color: white;
+            display: grid;
+        }
+        .itm img{
+            width: 400px;
+            height: 400px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        p{
+            text-align: center; font-size: 30PX;color: black; margin-top: 50px;
+        }
+        a{
+            /* text-decoration: underline; */
+        }
+        @media (max-width: 767px) {
+            .itm{
+                /* float: left; */
+
+            }
+            .itm img{
+                width: 350px;
+                height: 350px;
+            }
+        }
+    </style>
+         <h2><center>Welcome <?php echo"$name";?></center></h2>
+
+        <div class="itm" >
+
+            <img src="../img/delivery.gif" alt="" width="400" height="400">
+
+        </div>
+        <!-- <h2><center>your Location : <?php echo "$city"; ?></center></h2> -->
+        <div class="get">
+            <?php
+// Define the SQL query to fetch unassigned orders
+$sql12 = "SELECT
+    fd.Fid AS Fid,
+    fd.location AS cure,
+    fd.name AS donor_name,
+    fd.phoneno AS donor_phone,
+    fd.date AS donation_date,
+    fd.delivery_by,
+    fd.address AS From_address,
+    ad.name AS delivery_person_name,
+    ad.address AS To_address
+FROM
+    food_donations fd
+LEFT JOIN
+    `admin` ad ON fd.assigned_to = ad.Aid
+WHERE
+    fd.assigned_to IS NOT NULL
+    AND fd.delivery_by IS NULL
+    AND fd.location = '$city'
+";
+// Execute the query
+$result3=mysqli_query($connection, $sql12);
+// Check for errors
+if (!$result3) {
+    die("Error executing query: " . mysqli_error($conn));
+}
+
+
+// If the delivery person has taken an order, update the assigned_to field in the database
+if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
+    $order_id = $_POST['order_id'];
+    $delivery_person_id = $_POST['delivery_person_id'];
+    $sql = "SELECT * FROM `food_donations` WHERE Fid = $order_id AND `delivery_by` IS NOT NULL";
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // Order has already been assigned to someone else
+        die("Sorry, this order has already been assigned to someone else.");
+    }
+
+
+    $sql = "UPDATE `food_donations` SET `delivery_by` = $delivery_person_id WHERE Fid = $order_id";
+    // $result = mysqli_query($conn, $sql);
+    $result=mysqli_query($connection, $sql);
+
+
+    if (!$result) {
+        die("Error assigning order: " . mysqli_error($conn));
+    }
+
+    // Reload the page to prevent duplicate assignments
+    header('Location: ' . $_SERVER['REQUEST_URI']);
+    // exit;
+    ob_end_flush();
+}
+// mysqli_close($conn);
+
+
+?>
+<div class="log">
+<!-- <button type="submit" name="food" onclick="">My orders</button> -->
+<a href="deliverymyord.php" style="margin-bottom:10px;">My orders</a>
+
+        <table class="table">
+        <thead>
+        <tr>
+            <th >Name</th>
+            <th>phoneno</th>
+            <th>date/time</th>
+            <th>Pickup address</th>
+            <th>Delivery Address</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+            <tr>
+                <th >Shivam</th>
+                <th >9359596449</th>
+                <th >02/06/2025 07:40:16</th>
+                <th >Mhalgi nagar</th>
+                <th >Mahal</th>
+                <th ><a href="../index.php">Take order</a></th>
+            </tr>
+            </thead>
+            </table>
+
+</div>
+
+
+<!-- Display the orders in an HTML table -->
+<div class="table-container">
+         <!-- <p id="heading">donated</p> -->
+
+
+
+
+
+
+   <br>
+   <br>
+</body>
+</html>
---
+++ delivery/deliverymyord.php
@@ -0,0 +1,170 @@
+<?php
+ob_start();
+
+// $connection = mysqli_connect("localhost:3307", "root", "");
+// $db = mysqli_select_db($connection, 'demo');
+include '../connection.php';
+ include("connect.php");
+if($_SESSION['name']==''){
+	header("location:deliverylogin.php");
+}
+$name=$_SESSION['name'];
+$id=$_SESSION['Did'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="delivery.css">
+    <link rel="stylesheet" href="../home.css">
+</head>
+<body>
+<header>
+        <div class="logo">Food <b style="color: #06C167;">Donate</b></div>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="delivery.php">Home</a></li>
+                <li><a href="openmap.php">map</a></li>
+                <li><a href="deliverymyord.php"  class="active">myorders</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <script>
+        hamburger=document.querySelector(".hamburger");
+        hamburger.onclick =function(){
+            navBar=document.querySelector(".nav-bar");
+            navBar.classList.toggle("active");
+        }
+    </script>
+    <style>
+        .itm{
+            background-color: white;
+            display: grid;
+        }
+        .itm img{
+            width: 400px;
+            height: 400px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        p{
+            text-align: center; font-size: 28PX;color: black;
+        }
+        a{
+            /* text-decoration: underline; */
+        }
+        @media (max-width: 767px) {
+            .itm{
+                /* float: left; */
+
+            }
+            .itm img{
+                width: 350px;
+                height: 350px;
+            }
+        }
+    </style>
+
+        <div class="itm" >
+
+            <img src="../img/delivery.gif" alt="" width="400" height="400">
+
+        </div>
+
+        <div class="get">
+<?php
+// Define the SQL query to fetch unassigned orders
+$sql = "SELECT fd.Fid AS Fid, fd.name,fd.phoneno,fd.date,fd.delivery_by, fd.address as From_address,
+ad.name AS delivery_person_name, ad.address AS To_address
+FROM food_donations fd
+LEFT JOIN admin ad ON fd.assigned_to = ad.Aid where delivery_by='$id';
+";
+// Execute the query
+$result=mysqli_query($connection, $sql);
+// Check for errors
+if (!$result) {
+    die("Error executing query: " . mysqli_error($conn));
+}
+
+
+
+
+// If the delivery person has taken an order, update the assigned_to field in the database
+if (isset($_POST['food']) && isset($_POST['delivery_person_id'])) {
+    $order_id = $_POST['order_id'];
+    $delivery_person_id = $_POST['delivery_person_id'];
+
+    $sql = "UPDATE food_donations SET delivery_by = $delivery_person_id WHERE Fid = $order_id";
+    // $result = mysqli_query($conn, $sql);
+    $result=mysqli_query($connection, $sql);
+
+
+    if (!$result) {
+        die("Error assigning order: " . mysqli_error($conn));
+    }
+
+    // Reload the page to prevent duplicate assignments
+    header('Location: ' . $_SERVER['REQUEST_URI']);
+    // exit;
+    ob_end_flush();
+}
+// mysqli_close($conn);
+
+
+?>
+<div class="log">
+<!-- <button type="submit" name="food" onclick="">My orders</button> -->
+<a href="delivery.php">Take orders</a>
+<p>Order assigned to you</p>
+<br>
+</div>
+
+
+<!-- Display the orders in an HTML table -->
+<div class="table-container">
+         <!-- <p id="heading">donated</p> -->
+         <div class="table-wrapper">
+         <table class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>phoneno</th>
+            <th>date/time</th>
+            <th>Pickup address</th>
+            <th>Delivery Address</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+            <tr>
+                <th >Shivam</th>
+                <th >9359596449</th>
+                <th >02/06/2025 07:40:16</th>
+                <th >Mhalgi nagar</th>
+                <th >Mahal</th>
+                <th ><a href="../index.php">Take order</a></th>
+            </tr>
+            </thead>
+            </table>
+
+            </div>
+
+
+
+
+
+   <br>
+   <br>
+</body>
+</html>
